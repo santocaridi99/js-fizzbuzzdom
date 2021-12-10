9ae449847cbe9caac8f2f1fcd4c1d07d1ab8f461
santocaridi99 <santocaridi99@gmail.com>
2021-12-10 15:48:57 +0100

    seconda Milestone

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const outputHtml = document.querySelector('.conta')
+const outputHtml = document.querySelector('.container')
 //algoritmo fizzbuzz
 //ciclo deve contare numeri da 1 a 100
 //prima lavoro sull eccezione se numero è divisibile sia per 3 che per 5 allora stampo Fizzbuzz
@@ -7,15 +7,15 @@ const outputHtml = document.querySelector('.conta')
 //restante dei numeri stampati normalmente 
 for(let i=1 ; i<=100;i++){
     if(i % 3 === 0 && i % 5 === 0){
-        outputHtml.innerHTML += '<div>Fizzbuz</div>'
+        outputHtml.innerHTML += '<div class="box fizzBuzz">Fizzbuz</div>'
     }
     else if(i % 3 === 0){
-        outputHtml.innerHTML += '<div>Fizz</div>'
+        outputHtml.innerHTML += '<div class="box fizz">Fizz</div>'
     }
     else if(i % 5 ===0){
-        outputHtml.innerHTML += '<div>Buzz</div>'
+        outputHtml.innerHTML += '<div class="box buzz">Buzz</div>'
     }
     else{
-        outputHtml.innerHTML += `<div> ${i} </div>`
+        outputHtml.innerHTML += `<div class="box"> ${i} </div>`
     }
 }
